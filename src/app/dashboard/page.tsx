46a728d0aa059464e48239455940644e5e7dc119
Daniel Ezekiel <dbezekiel77@gmail.com>
2024-11-17 00:24:16 +0100

    chore: added login, dashboard pages and layout component, removed Next default setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "../_layouts/DashboardLayout";
+
+function Dashboard() {
+  return (
+    <DashboardLayout>
+      <h1>LendSqr Dashboard</h1>
+    </DashboardLayout>
+  );
+}
+
+export default Dashboard;
