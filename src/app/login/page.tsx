328073fb1ba33cd3e912178de951bfcb7aa2be1f
Daniel Ezekiel <dbezekiel77@gmail.com>
2024-11-17 19:53:57 +0100

    feat: UI for login page complete - fully responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/assets/images/logo.svg";
+import LoginImg from "@/assets/images/sign-in.svg";
 import styles from "./login.module.scss";
 function Login() {
-  return <div className={styles.page}>Login</div>;
+  return (
+    <div className={styles.page}>
+      <div className={styles.imagesContainer}>
+        <Link href={"/login"}>
+          <Image src={Logo} alt='Lendsqr Logo' className={styles.logo} />
+        </Link>
+
+        <div className={styles.loginImgContainer}>
+          <Image className={styles.loginImg} src={LoginImg} alt='Login Image' />
+        </div>
+      </div>
+
+      <div className={styles.formOverallContainer}>
+        <div className={styles.formContainer}>
+          <h1 className={styles.pageTitle}>Welcome!</h1>
+          <p className={styles.formInstruction}>Enter details to login</p>
+
+          <form action='' className={styles.form}>
+            <div className={styles.formControl}>
+              <input id='email' name='email' placeholder='Email' type='text' />
+            </div>
+
+            <div className={styles.formControl}>
+              <input
+                id='password'
+                name='password'
+                placeholder='Password'
+                type='password'
+              />
+            </div>
+            <Link href='' className={styles.passwordResetLink}>
+              Forgot Password?
+            </Link>
+
+            <button className={styles.submitButton} type='button'>
+              Log in
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import styles from "./page.module.scss";
+import styles from "./sass/page.module.scss";
 
 export default function Home() {
   return (
